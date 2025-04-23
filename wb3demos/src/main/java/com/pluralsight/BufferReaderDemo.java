8128ee6a4163229432029f56775d3c66f44b36be
thejaylajones <janeissaj@gmail.com>
2025-04-23 15:45:59 -0400

    end of day

---
+++ wb3demos/src/main/java/com/pluralsight/BufferReaderDemo.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferReaderDemo {
+
+    public static void main(String [] args) throws IOException {
+
+        FileReader fr = null;
+        fr = new FileReader("jokes.txt");
+
+        BufferedReader br = new BufferedReader(fr);
+
+        String currentLine;
+
+        while((currentLine = br.readLine()) != null){
+            System.out.println(currentLine);
+        }
+
+        br.close();
+    }
+}
